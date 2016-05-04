0efb45dfc34d7046ba4b2aac95ef479e2696a89e
Juan Bernardo <jrivera@hugeinc.com>
2016-05-04 18:35:11 -0500

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+
+const path = require('path');
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+const DEFAULT_PORT = process.env.PORT || 3000;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: 'style!css?sourceMap&modules!postcss!sass?sourceMap',
+        include: PATHS.app
+      },
+      {
+        test: /\.js$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+module.exports = merge(common, {
+  start: {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT || DEFAULT_PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  },
+  build: {
+
+  }
+}[TARGET]);
