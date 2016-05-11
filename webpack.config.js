b1c8db63bc9487a2cb2dd86eb8b9c0679b2b46b0
Luisa Vaca <gguevara@hugeinc.com>
2016-05-11 17:39:46 -0500

    added linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack');
+import webpack from 'webpack';
 
 const path = require('path');
 const merge = require('webpack-merge');
@@ -7,29 +7,29 @@ const TARGET = process.env.npm_lifecycle_event;
 const DEFAULT_PORT = process.env.PORT || 3000;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
 };
 
 const common = {
   entry: PATHS.app,
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: /\.scss$/,
         loader: 'style!css?sourceMap&modules!postcss!sass?sourceMap',
-        include: PATHS.app
+        include: PATHS.app,
       },
       {
         test: /\.js$/,
         loaders: ['babel?cacheDirectory'],
-        include: PATHS.app
-      }
-    ]
-  }
+        include: PATHS.app,
+      },
+    ],
+  },
 };
 
 module.exports = merge(common, {
@@ -42,13 +42,13 @@ module.exports = merge(common, {
       progress: true,
       stats: 'errors-only',
       host: process.env.HOST,
-      port: process.env.PORT || DEFAULT_PORT
+      port: process.env.PORT || DEFAULT_PORT,
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ]
+      new webpack.HotModuleReplacementPlugin(),
+    ],
   },
   build: {
 
-  }
+  },
 }[TARGET]);
