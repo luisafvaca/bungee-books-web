867cb5f81b05e015c898a10fcddf4564a38ceb43
Juan Bernardo <jrivera@hugeinc.com>
2016-05-11 18:37:25 -0500

    css styles

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {
+   Component
+} from 'react';
+import style from './App.scss';
+import classNames from 'classNames/bind';
 
-class App extends React.Component {
+const css = classNames.bind(style);
+
+class App extends Component {
   render () {
     return (
-      <div>
-        <p>Hola Mundo</p>
+      <div className={ style.app + ' ' + style['app-coso'] }>
+        <p className={ css('coso') }>Hola Mundo</p>
       </div>
     );
   }
